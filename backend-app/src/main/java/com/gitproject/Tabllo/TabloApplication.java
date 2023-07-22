406829817cbb0753281cfbda3d67d9631e841760
stefan22 <s3ri0us22@gmail.com>
2023-07-22 17:19:36 +0200

    Initialize backend-app project

---
+++ backend-app/src/main/java/com/gitproject/Tabllo/TabloApplication.java
@@ -0,0 +1,13 @@
+package com.gitproject.Tabllo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabloApplication.class, args);
+	}
+
+}
---
+++ backend-app/src/test/java/com/gitproject/Tabllo/TabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitproject.Tabllo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
